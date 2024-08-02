a67164715cb3424ff193b10016421c8236b4f316
IanGilSoria1 <81983006+IanGilSoria1@users.noreply.github.com>
2024-08-02 08:59:58 -0300

    main (1).py
    Feat: lecturas analogicas ADS1115

--- actividades_python/ej_04/main (1).py
+++ actividades_python/ej_04/main (1).py
@@ -1,10 +1,10 @@
 import gpiozero
 import Adafruit_ADS1x15
 
-# Crear una instancia de ADC
+# Crear condicion del ADC
 adc = Adafruit_ADS1x15.ADS1115()
 
-# Configuración de los pines GPIO para los LEDs
+# Asignacion de pines para LEDs
 red_led = PWMLED(17)
 blue_led = PWMLED(27)
 
@@ -22,16 +22,16 @@ def main():
         	time.sleep(1)
 
 def read_potentiometer():
-    	# Leer valor del potenciometro
+    	# se lee el pote
     	value = adc.read_adc(0, gain=GAIN)
-	# Escalar el valor a un rango de 0 a 30 grados
+	# se escala el valor en un rango de 0 a 30 grados
     	temperature_setpoint = value * 30.0 / 32767.0
     	return temperature_setpoint
 
 def read_thermistor():
-   	# Leer valor del termistor
+   	# se lee el termistor
 	value = adc.read_adc(1, gain=GAIN)
-	# Convertir el valor del termistor a grados centígrados usando la ecuación de Steinhart-Hart
+	# se transforma el valor del termistor a C° usando la ecuación de Steinhart-Hart
 	resistance = R_REF * (32767.0 / value - 1.0)
 	temperature = 1.0 / (1.0 / 298.15 + (1.0 / BETA) * (resistance / R_REF - 1.0)) - 273.15
 	return temperature
@@ -42,11 +42,11 @@ def control_leds(temp_setpoint, actual_temp):
 	duty_cycle = min(abs(difference) / max_difference, 1.0)
 
 	if difference > 0:
-		# Temperatura real está por encima del setpoint, encender LED azul
+		# Temperatura real está < que el setpoint, lED blue on
 		blue_led.value = duty_cycle
         	red_led.value = 0
     	else:
-        	# Temperatura real está por debajo del setpoint, encender LED rojo
+        	# Temp real > que el setpoint, LED red on
         	red_led.value = duty_cycle
         	blue_led.value = 0
 
