cd9155847978dcfb19dc51499c02e01131495765
IanGilSoria1 <81983006+IanGilSoria1@users.noreply.github.com>
2024-08-02 08:42:02 -0300

    Update main.py

--- actividades_python/ej_03/main.py
+++ actividades_python/ej_03/main.py
@@ -6,11 +6,11 @@ led_rojo = LED(19)
 led_verde = LED(26)
 led_azul = LED(13)
 while True:
-	comando = input("Ingrese comando: buzz o rgb: ")
+	comando = input("Se ingresa las ordenes de buzz/rgb: ")
 	if comando == "rgb":
-		opcion = input("Ingrese opcion: red, green or blue: ")
+		opcion = input("Se diferencian entre cada LED")
 	elif comando == "buzz":
-		opcion = input("Ingrese opcion: on u off: ")		
+		opcion = input("Selecciona El encendido/apagado")		
 
 	if comando == "buzz":
 		if opcion == "on":
